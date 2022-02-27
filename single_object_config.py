889efe2cfe97a3ea67d622767d247da3254135ad
Hollow_Mike <saichandgubbala@gmail.com>
2022-02-27 17:51:50 +0530

    Create single_object_config.py

---
+++ single_object_config.py
@@ -0,0 +1,23 @@
+# -----------------------------
+#   IMPORTS
+# -----------------------------
+# Import the necessary packages
+import os
+
+# base path directory
+path_dir = "/content/object-tracking-dlib"
+
+# define the directory path to the input videos
+input_dir = os.path.join(path_dir, "input")
+
+# define the directory path to the output videos
+output_dir = os.path.join(path_dir, "output")
+
+# define the directory path to the mobilenet model
+cnn_caffe_dir = os.path.join(path_dir, "mobilenet_ssd")
+
+# define the class label which is of interest
+label = "person"
+
+# define threshold confidence to filter weak detections
+thres_confidence = 0.2
