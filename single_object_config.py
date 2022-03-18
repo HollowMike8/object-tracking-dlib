8ef3d9d63eb1df09aed088b7d7dc5d2b419bf9bc
Hollow_Mike <saichandgubbala@gmail.com>
2022-03-18 12:31:05 +0530

    Update single_object_config.py

--- single_object_config.py
+++ single_object_config.py
@@ -3,9 +3,15 @@
 # -----------------------------
 # Import the necessary packages
 import os
+from sys import platform
+from IPython import get_ipython
 
 # base path directory
-path_dir = "/content/object-tracking-dlib"
+if platform == "win32":
+    path_dir = os.path.dirname(__file__)
+    
+elif 'google.colab' in str(get_ipython()):
+    path_dir = "/content/object-tracking-dlib"
 
 # define the directory path to the input videos
 input_dir = os.path.join(path_dir, "input")
@@ -15,6 +21,9 @@ output_dir = os.path.join(path_dir, "output")
 
 # define the directory path to the mobilenet model
 cnn_caffe_dir = os.path.join(path_dir, "mobilenet_ssd")
+
+# define the directory path to the yolo-coco model
+cnn_yolo_dir = os.path.join(path_dir, "yolo-coco")
 
 # define the class label which is of interest
 label = "person"
